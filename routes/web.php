28617e45a2e7e6c70d440aa132e617487e42e490
Rizki Rohman <rizkirohman54@gmail.com>
2026-09-11 09:30:29 +0700

    Menyederhanakan route di web.php

--- routes/web.php
+++ routes/web.php
@@ -28,40 +28,59 @@ Route::get('/admin-test', function () {
     return 'Halaman khusus Admin Kepegawaian';
 })->middleware(['auth', 'admin']);
 
-Route::resource('unit-kerja', UnitKerjaController::class)->middleware(['auth', 'admin']);;
+Route::resource('unit-kerja', UnitKerjaController::class)
+    ->middleware(['auth', 'admin']);;
 
-Route::resource('jabatan', JabatanController::class)->middleware(['auth', 'admin']);
+Route::resource('jabatan', JabatanController::class)
+    ->middleware(['auth', 'admin']);
 
 Route::resource('pegawai', PegawaiController::class)
     ->middleware(['auth', 'admin']);
 
-Route::get('pegawai/{pegawai}/riwayat-pendidikan', [RiwayatPendidikanController::class, 'index'])
-    ->middleware(['auth', 'admin'])
-    ->name('pegawai.riwayat-pendidikan.index');
-
-Route::get('pegawai/{pegawai}/riwayat-pendidikan/create', [RiwayatPendidikanController::class, 'create'])
-    ->middleware(['auth', 'admin'])
-    ->name('pegawai.riwayat-pendidikan.create');
-
-Route::post('pegawai/{pegawai}/riwayat-pendidikan', [RiwayatPendidikanController::class, 'store'])
-    ->middleware(['auth', 'admin'])
-    ->name('pegawai.riwayat-pendidikan.store');
-
-Route::get('pegawai/{pegawai}/riwayat-pendidikan/{riwayatPendidikan}/edit', [RiwayatPendidikanController::class, 'edit'])
-    ->middleware(['auth', 'admin'])
-    ->name('pegawai.riwayat-pendidikan.edit');
-
-Route::put('pegawai/{pegawai}/riwayat-pendidikan/{riwayatPendidikan}', [RiwayatPendidikanController::class, 'update'])
-    ->middleware(['auth', 'admin'])
-    ->name('pegawai.riwayat-pendidikan.update');
-
-Route::delete('pegawai/{pegawai}/riwayat-pendidikan/{riwayatPendidikan}', [RiwayatPendidikanController::class, 'destroy'])
-    ->middleware(['auth', 'admin'])
-    ->name('pegawai.riwayat-pendidikan.destroy');
+Route::resource('pegawai.riwayat-pendidikan', RiwayatPendidikanController::class)
+    ->middleware(['auth', 'admin']);
 
 Route::resource('pegawai.riwayat-pangkat', RiwayatPangkatController::class)
     ->middleware(['auth', 'admin'])
     ->except(['show']);
+
+Route::resource('pegawai.riwayat-jabatan', RiwayatJabatanController::class)
+    ->middleware(['auth', 'admin']);
+
+Route::resource('pegawai.dokumen', DokumenPegawaiController::class)
+    ->parameters(['dokumen' => 'dokumen',])
+    ->middleware(['auth', 'admin'])
+    ->except(['show']);
+
+Route::get('pegawai/{pegawai}/dokumen/{dokumen}/download', [DokumenPegawaiController::class, 'download'])
+    ->name('pegawai.dokumen.download')
+    ->middleware(['auth', 'admin']);
+
+// Route::get('pegawai/{pegawai}/riwayat-pendidikan', [RiwayatPendidikanController::class, 'index'])
+//     ->middleware(['auth', 'admin'])
+//     ->name('pegawai.riwayat-pendidikan.index');
+
+// Route::get('pegawai/{pegawai}/riwayat-pendidikan/create', [RiwayatPendidikanController::class, 'create'])
+//     ->middleware(['auth', 'admin'])
+//     ->name('pegawai.riwayat-pendidikan.create');
+
+// Route::post('pegawai/{pegawai}/riwayat-pendidikan', [RiwayatPendidikanController::class, 'store'])
+//     ->middleware(['auth', 'admin'])
+//     ->name('pegawai.riwayat-pendidikan.store');
+
+// Route::get('pegawai/{pegawai}/riwayat-pendidikan/{riwayatPendidikan}/edit', [RiwayatPendidikanController::class, 'edit'])
+//     ->middleware(['auth', 'admin'])
+//     ->name('pegawai.riwayat-pendidikan.edit');
+
+// Route::put('pegawai/{pegawai}/riwayat-pendidikan/{riwayatPendidikan}', [RiwayatPendidikanController::class, 'update'])
+//     ->middleware(['auth', 'admin'])
+//     ->name('pegawai.riwayat-pendidikan.update');
+
+// Route::delete('pegawai/{pegawai}/riwayat-pendidikan/{riwayatPendidikan}', [RiwayatPendidikanController::class, 'destroy'])
+//     ->middleware(['auth', 'admin'])
+//     ->name('pegawai.riwayat-pendidikan.destroy');
+
+
 
 // Route::get('pegawai/{pegawai}/riwayat-jabatan', [RiwayatJabatanController::class, 'index'])
 //     ->name('pegawai.riwayat-jabatan.index')
@@ -87,33 +106,42 @@ Route::resource('pegawai.riwayat-pangkat', RiwayatPangkatController::class)
 //     ->name('pegawai.riwayat-jabatan.destroy')
 //     ->middleware(['auth', 'admin']);
 
-Route::resource('pegawai.riwayat-jabatan', RiwayatJabatanController::class)
-    ->middleware(['auth', 'admin']);
 
-Route::get('pegawai/{pegawai}/dokumen', [DokumenPegawaiController::class, 'index'])
-    ->name('pegawai.dokumen.index')
-    ->middleware(['auth', 'admin']);
 
-Route::get('pegawai/{pegawai}/dokumen/create', [DokumenPegawaiController::class, 'create'])
-    ->name('pegawai.dokumen.create')
-    ->middleware(['auth', 'admin']);
+// Route::middleware(['auth', 'admin'])->group(function () {
+//     // 1. Rute Kustom (Download)
+//     Route::get('pegawai/{pegawai}/dokumen/{dokumen}/download', [DokumenPegawaiController::class, 'download'])
+//         ->name('pegawai.dokumen.download');
 
-Route::post('pegawai/{pegawai}/dokumen', [DokumenPegawaiController::class, 'store'])
-    ->name('pegawai.dokumen.store')
-    ->middleware(['auth', 'admin']);
+//     // 2. Resource Routes (index, create, store, edit, update, destroy)
+//     Route::resource('pegawai.dokumen', DokumenPegawaiController::class);
+// });
 
-Route::get('pegawai/{pegawai}/dokumen/{dokumen}/download', [DokumenPegawaiController::class, 'download'])
-    ->name('pegawai.dokumen.download')
-    ->middleware(['auth', 'admin']);
 
-Route::get('pegawai/{pegawai}/dokumen/{dokumen}/edit', [DokumenPegawaiController::class, 'edit'])
-    ->name('pegawai.dokumen.edit')
-    ->middleware(['auth', 'admin']);
+// Route::get('pegawai/{pegawai}/dokumen', [DokumenPegawaiController::class, 'index'])
+//     ->name('pegawai.dokumen.index')
+//     ->middleware(['auth', 'admin']);
 
-Route::put('pegawai/{pegawai}/dokumen/{dokumen}', [DokumenPegawaiController::class, 'update'])
-    ->name('pegawai.dokumen.update')
-    ->middleware(['auth', 'admin']);
+// Route::get('pegawai/{pegawai}/dokumen/create', [DokumenPegawaiController::class, 'create'])
+//     ->name('pegawai.dokumen.create')
+//     ->middleware(['auth', 'admin']);
 
-Route::delete('pegawai/{pegawai}/dokumen/{dokumen}', [DokumenPegawaiController::class, 'destroy'])
-    ->name('pegawai.dokumen.destroy')
-    ->middleware(['auth', 'admin']);
+// Route::post('pegawai/{pegawai}/dokumen', [DokumenPegawaiController::class, 'store'])
+//     ->name('pegawai.dokumen.store')
+//     ->middleware(['auth', 'admin']);
+
+// Route::get('pegawai/{pegawai}/dokumen/{dokumen}/download', [DokumenPegawaiController::class, 'download'])
+//     ->name('pegawai.dokumen.download')
+//     ->middleware(['auth', 'admin']);
+
+// Route::get('pegawai/{pegawai}/dokumen/{dokumen}/edit', [DokumenPegawaiController::class, 'edit'])
+//     ->name('pegawai.dokumen.edit')
+//     ->middleware(['auth', 'admin']);
+
+// Route::put('pegawai/{pegawai}/dokumen/{dokumen}', [DokumenPegawaiController::class, 'update'])
+//     ->name('pegawai.dokumen.update')
+//     ->middleware(['auth', 'admin']);
+
+// Route::delete('pegawai/{pegawai}/dokumen/{dokumen}', [DokumenPegawaiController::class, 'destroy'])
+//     ->name('pegawai.dokumen.destroy')
+//     ->middleware(['auth', 'admin']);
